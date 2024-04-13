90e910b7c1a8af8074e110b65edef47d2b74e420
cindy-x-liang <67083541+cindy-x-liang@users.noreply.github.com>
2024-04-13 15:43:54 -0400

    new dataset with categories'

--- backend/app.py
+++ backend/app.py
@@ -28,7 +28,7 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 Loading in data -- Start
 """
 # Specify the path to the JSON file relative to the current script
-json_file_path = os.path.join(current_directory, 'results.json')
+json_file_path = os.path.join(current_directory, 'results_categories.json')
 
 # Assuming your JSON data is stored in a file named 'init.json'
 # with open(json_file_path, 'r') as file:
